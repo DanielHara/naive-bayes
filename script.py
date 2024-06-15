9edc54d36a7aba49749270ba6265b04cf5ed9f2b
Daniel Hara <haradaniel734@gmail.com>
2024-06-15 15:22:21 +0200

    downlaod the dataset

--- script.py
+++ script.py
@@ -1,10 +1,7 @@
-from dotenv import load_dotenv
-import os
+import opendatasets as od
 
-load_dotenv()
+dataset_url = 'https://www.kaggle.com/datasets/abdallahwagih/amazon-reviews'
+od.download(dataset_url)
 
-open_datasets_username = os.getenv("OPEN_DATASETS_USERNAME")
-open_datasets_key = os.getenv("OPEN_DATASETS_KEY")
 
-print('open_datasets_username', open_datasets_username)
-print('open_datasets_key', open_datasets_key)
+
