16790aefb09102aeeed3e8a1fa47a07fe1d25a8d
Daniel Hara <haradaniel734@gmail.com>
2024-06-15 16:29:16 +0200

    preparing calculation

--- script.py
+++ script.py
@@ -15,6 +15,13 @@ frequency_stars = {}
 
 number_reviews_containing_word_given_stars = {}
 
+def get_distinct_words_from_review_text(reviewText):
+    words = re.split('[^a-zA-Z]', review_text)
+    distinct_words = set(map(lambda word: word.lower(), words))
+
+    return distinct_words
+
+
 with open("amazon-reviews/Cell_Phones_and_Accessories_5.json", "r") as file:
     for line in file:
         review = json.loads(line)
@@ -23,8 +30,7 @@ with open("amazon-reviews/Cell_Phones_and_Accessories_5.json", "r") as file:
         frequency_stars[review_stars] = frequency_stars.get(review_stars, 0) + 1
 
         review_text = review['reviewText']
-        words = re.split('[^a-zA-Z]', review_text)
-        distinct_words = set(map(lambda word: word.lower(), words))
+        distinct_words = get_distinct_words_from_review_text(review_text)
 
         for word in distinct_words:
             dictionary_set.add(word)
@@ -37,9 +43,23 @@ with open("amazon-reviews/Cell_Phones_and_Accessories_5.json", "r") as file:
         
         total_reviews = total_reviews + 1
 
+
+
+probability_reviews_containing_word_given_stars = number_reviews_containing_word_given_stars
+for [review_stars, number_reviews_containing_word_dict] in probability_reviews_containing_word_given_stars.items():
+    total_reviews_with_review_starts = frequency_stars.get(review_stars, 0)
+
+    for word in number_reviews_containing_word_dict.keys():
+        number_reviews_containing_word_dict[word] = number_reviews_containing_word_dict[word] / total_reviews_with_review_starts
+
+
+review = "Needed an upgrade from my io7 haha. Not holding its charge and it was time. Not the type of person that needs the newest toy. If it works why change."
+distinct_words = get_distinct_words_from_review_text(review)
+
 print(total_reviews)
 print(frequency_stars)
 
 print(len(dictionary_set))
 
-print(number_reviews_containing_word_given_stars)
+# print(number_reviews_containing_word_given_stars)
+print(probability_reviews_containing_word_given_stars)
