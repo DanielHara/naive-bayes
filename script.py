455a4c91f09ef7e7a9d7e9699791374bc7c20b33
Daniel Hara <haradaniel734@gmail.com>
2024-06-15 16:16:26 +0200

    counting number of reviews given a word

--- script.py
+++ script.py
@@ -12,6 +12,9 @@ probabilities_dict = {}
 
 total_reviews = 0
 frequency_stars = {}
+
+number_reviews_containing_word_given_stars = {}
+
 with open("amazon-reviews/Cell_Phones_and_Accessories_5.json", "r") as file:
     for line in file:
         review = json.loads(line)
@@ -20,10 +23,17 @@ with open("amazon-reviews/Cell_Phones_and_Accessories_5.json", "r") as file:
         frequency_stars[review_stars] = frequency_stars.get(review_stars, 0) + 1
 
         review_text = review['reviewText']
-        words = re.split('[^a-zA-Z0-9]', review_text)
+        words = re.split('[^a-zA-Z]', review_text)
+        distinct_words = set(map(lambda word: word.lower(), words))
 
-        for word in words:
-            dictionary_set.add(word.lower())
+        for word in distinct_words:
+            dictionary_set.add(word)
+        
+        for word in distinct_words:
+            if review_stars not in number_reviews_containing_word_given_stars:
+                number_reviews_containing_word_given_stars[review_stars] = {}
+
+            number_reviews_containing_word_given_stars[review_stars][word] = number_reviews_containing_word_given_stars[review_stars].get(word, 0) + 1
         
         total_reviews = total_reviews + 1
 
@@ -31,3 +41,5 @@ print(total_reviews)
 print(frequency_stars)
 
 print(len(dictionary_set))
+
+print(number_reviews_containing_word_given_stars)
