00c87668974edd7fa5a8ab80c8b7772561360a7c
Daniel Hara <haradaniel734@gmail.com>
2024-06-15 16:04:08 +0200

    count frequency_stars

--- script.py
+++ script.py
@@ -7,15 +7,23 @@ od.download(dataset_url)
 
 # Retrieve JSON data from the file
 dictionary_set = set()
+
+probabilities_dict = {}
+
+frequency_stars = {}
 with open("amazon-reviews/Cell_Phones_and_Accessories_5.json", "r") as file:
     for line in file:
-        data = json.loads(line)
+        review = json.loads(line)
 
-        reviewText = data['reviewText']
-        
-        words = re.split('[^a-zA-Z0-9]', reviewText)
+        review_stars = int(review['overall'])
+        frequency_stars[review_stars] = frequency_stars.get(review_stars, 0) + 1
+
+        review_text = review['reviewText']
+        words = re.split('[^a-zA-Z0-9]', review_text)
 
         for word in words:
             dictionary_set.add(word.lower())
+
+print(frequency_stars)
 
 print(len(dictionary_set))
