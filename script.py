00951adddccd62e66182360813a201a64d9322f4
Daniel Hara <haradaniel734@gmail.com>
2024-06-15 15:11:08 +0200

    set up keys

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
-opendatasets
+from dotenv import load_dotenv
+import os
 
-username = 'danielhara'
-key = 'my_key'
+load_dotenv()
 
+open_datasets_username = os.getenv("OPEN_DATASETS_USERNAME")
+open_datasets_key = os.getenv("OPEN_DATASETS_KEY")
 
+print('open_datasets_username', open_datasets_username)
+print('open_datasets_key', open_datasets_key)
